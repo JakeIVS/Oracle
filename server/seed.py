f41f7f417155dcd4044a34d2482b385e15b5f750
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-25 12:23:59 -0600

    seed all spell data from public API

--- server/seed.py
+++ server/seed.py
@@ -1,11 +1,17 @@
 import requests
 from models import *
+from config import db, app
 
+def data_wipe():
+    print("Clearing old data...")
+    Spell.query.delete()
+    print('Complete.')
 
 def seed_spells():
     print('Generating spells...')
     response = requests.get('https://www.dnd5eapi.co/api/spells').json()
     spell_list = response['results']
+    seeded_spells = []
     for spell in spell_list:
         url = spell['url']
         data = requests.get(f'https://www.dnd5eapi.co{url}').json()
@@ -18,12 +24,21 @@ def seed_spells():
             spell_range = data['range'],
             duration = data['duration'],
             components = ", ".join(data['components']),
-            material = data['material'],
             is_ritual = 0 if not data['ritual'] else 1,
             requires_concentration = 0 if not data['concentration'] else 1,
             casting_time = data['casting_time'],
             url = data['url']
         )
+        db.session.add(new_spell)
+        db.session.commit()
+        seeded_spells.append(new_spell)
+    print(f'Complete: {len(seeded_spells)} spells seeded.')
+
 
 def seed_items():
     pass
+
+if __name__ == '__main__':
+    with app.app_context():
+        data_wipe()
+        seed_spells()
