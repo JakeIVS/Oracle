1c24f9431772887724e23925d73b9af8100a08a8
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-26 14:00:10 -0600

    set up pallette colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#004E7A',
+        secondary: '#7799AC',
+        n: {
+          dark: '#B0B0B0',
+          light: '#D9D9D9',
+        },
+      },
+    },
   },
   plugins: [],
 };
