ac6361fce255e840589810d1c9081312038c4860
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-26 13:10:39 -0600

    format static header layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import './index.css';
 import CharacterSheet from './components/CharacterSheet';
+import Header from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <CharacterSheet />
-    </>
+    <div>
+      <Header />
+      <div className="h-screen pt-16">
+        <CharacterSheet />
+      </div>
+    </div>
   );
 }
 
