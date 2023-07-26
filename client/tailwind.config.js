c0499137a87ee9ca87e499bc2250e0cbdabda809
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-26 11:06:35 -0600

    create custom aspect ratio for character sheets

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        csheet: '9 / 6',
+      },
+    },
   },
   plugins: [],
-}
-
+};
