d84906452a518602e5561eba4ea98ac02fb109e5
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-27 15:01:30 -0600

    create signin form using formik

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './index.css';
 import CharacterSheet from './components/CharacterSheet';
 import Header from './components/Header';
+import SignupForm from './components/SignupForm';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,7 @@ function App() {
     <div>
       <Header />
       <div className="h-screen pt-16">
-        <CharacterSheet />
+        <SignupForm />
       </div>
     </div>
   );
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,23 +1,53 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, Field, Form, ErrorMessage, useField } from 'formik';
 import * as Yup from 'yup';
 
 function SignupForm() {
+  const field = '';
+  const errorField = '';
+
   return (
-    <Formik
-      initialValues={{ firstName: '', lastName: '', email: '', password: '' }}
-      validationSchema={Yup.object({
-        firstName: Yup.string().required('Required'),
-        lastName: Yup.string().required('Required'),
-        email: Yup.string()
-          .email('Invalid email address.')
-          .required('required'),
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    ></Formik>
+    <div className="h-full bg-gradient-to-t from-primary to-secondary">
+      <Formik
+        initialValues={{ firstName: '', lastName: '', email: '', password: '' }}
+        validationSchema={Yup.object({
+          firstName: Yup.string().required('Required'),
+          lastName: Yup.string().required('Required'),
+          email: Yup.string()
+            .email('Invalid email address.')
+            .required('required'),
+          password: Yup.string()
+            .min(6, 'Password must be 6-20 characters long')
+            .max(20, 'Password must be 6-20 characters long')
+            .required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form className="flex flex-col place-content-center justify-center px-[30%] pt-10">
+          <h1 className="pb-6 text-center font-serif text-2xl font-semibold text-white">
+            {' '}
+            Sign Up
+          </h1>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" type="text" />
+          <ErrorMessage name="firstName" />
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" type="text" />
+          <ErrorMessage name="lastName" />
+          <label htmlFor="email">Email</label>
+          <Field name="email" type="email" />
+          <ErrorMessage name="email" />
+          <label htmlFor="password">Password</label>
+          <Field name="password" type="password" />
+          <ErrorMessage name="password" />
+        </Form>
+      </Formik>
+    </div>
   );
 }
+
+export default SignupForm;
