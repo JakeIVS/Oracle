56559d85af3524552e90b619170fa91c28ef5be5
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-28 13:19:47 -0600

    format nav text

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,17 @@
 function Header() {
   return (
     <div className="absolute left-0 top-0 flex h-16 w-screen justify-center bg-primary px-3 pt-3">
-      <h1 className="absolute left-2 font-serif text-4xl font-bold text-n-light">
+      <h1 className="absolute left-2 font-serif text-4xl font-bold text-white">
         Oracle
       </h1>
-      <nav>Nav</nav>
+      <nav className="font-serif text-white">Nav</nav>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="absolute right-2 h-10 w-10 stroke-n-light stroke-1 transition-all duration-300 hover:stroke-white"
+        className="absolute right-2 h-10 w-10 stroke-white stroke-1 transition-all duration-300 hover:stroke-2"
       >
         <path
           strokeLinecap="round"
