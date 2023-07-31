1634d7990df77c97cc94f9805d09b540278e0e6c
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-31 09:25:11 -0600

    make character list component

---
+++ client/src/components/CharacterList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CharacterList() {
+  return (
+    <div className="h-full w-full bg-green-300">
+      <div>sdflkjslkdfjosaijf</div>
+    </div>
+  );
+}
+
+export default CharacterList;
