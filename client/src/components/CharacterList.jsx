80eb13906849f0d503e37f646ae617db5867a288
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-31 12:44:40 -0600

    add title to div

--- client/src/components/CharacterList.jsx
+++ client/src/components/CharacterList.jsx
@@ -4,7 +4,10 @@ import CharacterCard from './CharacterCard';
 function CharacterList(user) {
   return (
     <div className="h-full w-full">
-      <div className=" mx-[15%] grid grid-cols-1 gap-x-6 gap-y-3 pt-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+      <h1 className="pt-6 text-center font-serif text-3xl font-bold">
+        Your Characters
+      </h1>
+      <div className=" mx-[15%] grid grid-cols-1 gap-x-8 gap-y-3 pt-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         <CharacterCard />
         <CharacterCard />
         <CharacterCard />
