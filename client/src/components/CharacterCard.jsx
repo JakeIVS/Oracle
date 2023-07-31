1080aec5c324bfb6261b79ef0a1b7475176001fe
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-31 10:57:44 -0600

    create character card component

---
+++ client/src/components/CharacterCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CharacterCard() {
+  return (
+    <div className="rounded-lg bg-red-300">
+      <h2>Name</h2>
+      <h3>Level and Class</h3>
+      <h3>Campaign</h3>
+    </div>
+  );
+}
+
+export default CharacterCard;
