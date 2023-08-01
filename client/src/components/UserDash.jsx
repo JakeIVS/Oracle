3b19fe1161a4f8af19a3dcf2bfa131a133c11e9e
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-01 13:28:02 -0600

    add onClick to navigate to character list

--- client/src/components/UserDash.jsx
+++ client/src/components/UserDash.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function UserDash(user) {
+  const navigate = useNavigate();
+
+  function characterList() {
+    navigate('/characters', { replace: false });
+  }
+
   return (
     <div className="flex h-full w-full place-content-center bg-slate-300 p-20">
       <div className="grid aspect-square h-full grid-cols-4 gap-2 bg-slate-600 p-2 text-lg text-white">
         <h2 className="col-span-4 self-center text-center font-serif text-xl text-white">
           Welcome, {user.firstName}
         </h2>
-        <div className="col-span-3 flex flex-col-reverse outline outline-2">
+        <div
+          className="col-span-3 flex flex-col-reverse outline outline-2"
+          onClick={characterList}
+        >
           My Characters
         </div>
         <div className="flex max-w-prose flex-col-reverse justify-evenly text-center text-sm outline outline-2">
