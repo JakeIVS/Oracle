a7eea17c04132190ffd8e65408ff42259c5cb572
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-03 12:01:44 -0600

    make temporary landing page to navigate to sign up or sign in

--- client/src/components/HomeScreen.jsx
+++ client/src/components/HomeScreen.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function HomeScreen() {
+  const navigate = useNavigate();
+
   return (
-    <div className="h-full w-full bg-gradient-to-t from-n-dark to-secondary">
+    <div className="h-full w-full bg-gradient-to-t from-n-dark to-primary">
       <h1>You are not signed in</h1>
       <div>
-        <button>Sign In</button>
-        <button>Sign Up</button>
+        <button
+          className="button mx-2"
+          onClick={() => navigate('/login', { replace: true })}
+        >
+          Sign In
+        </button>
+        <button
+          className="button mx-2"
+          onClick={() => navigate('/signup', { replace: true })}
+        >
+          Sign Up
+        </button>
       </div>
     </div>
   );
