e7c3708db69435ac73b7c8345695d5264ffd5bbd
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-03 11:41:41 -0600

    position Nav in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,22 @@
-function Header() {
+function Header(user) {
   return (
-    <div className="absolute left-0 top-0 flex h-16 w-screen justify-center bg-primary px-3 pt-3">
+    <div className="absolute left-0 top-0 flex h-16 w-screen justify-center bg-primary px-3">
       <a
         href="/"
-        className="absolute left-2 font-serif text-4xl font-bold text-white"
+        className="absolute left-2 top-3 font-serif text-4xl font-bold text-white"
       >
         Oracle
       </a>
-      <nav className="font-serif text-white">Nav</nav>
+      <nav className="flex  flex-col font-serif text-white">
+        <div className="h-full w-full bg-secondary px-3 pt-5">Nav</div>
+      </nav>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="absolute right-2 h-10 w-10 stroke-white stroke-1 transition-all duration-300 hover:stroke-2"
+        className="absolute right-2 top-3 h-10 w-10 stroke-white stroke-1 transition-all duration-300 hover:stroke-2"
       >
         <path
           strokeLinecap="round"
