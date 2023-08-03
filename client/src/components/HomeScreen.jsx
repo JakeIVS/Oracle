676e67f60e90fc7948768ee08f25e7e06e487170
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-03 11:54:29 -0600

    create landing page

---
+++ client/src/components/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomeScreen() {
+  return (
+    <div className="h-full w-full bg-gradient-to-t from-n-dark to-secondary">
+      <h1>You are not signed in</h1>
+      <div>
+        <button>Sign In</button>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
+}
