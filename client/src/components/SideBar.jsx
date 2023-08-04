529fe5683fc27f222b8f93381fa8880046ccd616
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-04 15:01:13 -0600

    add portrait and account name, logged out state

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function SideBar({ sideBar }) {
-  console.log(sideBar);
+function SideBar({ sideBar, user }) {
+  const navigate = useNavigate();
   const shown =
     'absolute right-0 top-0 flex h-full w-1/3 flex-col bg-neutral-800 text-white transition-all';
   const hidden = 'absolute right-1 top-0 h-full w-0 transition-all';
+
+  console.log(user);
+
+  if (!user) {
+    return (
+      <div className={!sideBar ? hidden : shown}>
+        <div className={!sideBar ? 'hidden' : null}>
+          <h2 className="text-center text-3xl text-white">No User Logged In</h2>
+          <nav>
+            <div
+              className="bg-black text-center hover:bg-neutral-700"
+              onClick={() => navigate('/login', { replace: false })}
+            >
+              Log in
+            </div>
+            <div
+              className="bg-black text-center hover:bg-neutral-700"
+              onClick={() => navigate('/signup', { replace: false })}
+            >
+              Sign Up
+            </div>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={!sideBar ? hidden : shown}>
       <div className={!sideBar ? 'hidden' : null}>
-        <h1 className={'bg-black text-center'}>Sidebar</h1>
+        <div className="flex flex-col justify-center gap-3 p-16 px-24">
+          <img
+            src="/src/assets/placeholder-portrait.jpg"
+            alt="Profile Image"
+            className="aspect-square h-1/2 rounded-full"
+          />
+          <h2 className="text-center text-lg font-semibold">
+            {user.first_name} {user.last_name}
+          </h2>
+        </div>
         <nav>
           <div
             className="bg-black text-center hover:bg-neutral-700"
