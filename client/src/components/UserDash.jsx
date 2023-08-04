4f6b77b83bf04b6f5b98f52002336255f82e01e4
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-04 11:00:36 -0600

    thin borders around selections

--- client/src/components/UserDash.jsx
+++ client/src/components/UserDash.jsx
@@ -14,12 +14,12 @@ function UserDash({ id, firstName, lastName }) {
 
   return (
     <div className="flex h-full w-full place-content-center bg-slate-300 p-20">
-      <div className="grid aspect-square h-full grid-cols-4 gap-6 bg-slate-600 p-6 text-lg text-white">
+      <div className="grid aspect-square h-full grid-cols-4 gap-3 bg-slate-600 p-6 text-lg text-white">
         <h2 className="col-span-4 self-center text-center font-serif text-2xl font-semibold text-white">
           Welcome, {firstName}
         </h2>
         <div
-          className="group col-span-3 flex flex-col-reverse overflow-x-hidden overflow-y-hidden outline outline-4 outline-n-dark"
+          className="group col-span-3 flex flex-col-reverse overflow-x-hidden overflow-y-hidden outline outline-1 outline-n-dark"
           onClick={characterList}
         >
           <img
@@ -32,12 +32,12 @@ function UserDash({ id, firstName, lastName }) {
           </h3>
         </div>
         <div
-          className="transition-color flex flex-col-reverse justify-evenly overflow-x-hidden overflow-y-hidden bg-primary text-center outline outline-4 outline-n-dark duration-200 hover:text-xl"
+          className="transition-color flex flex-col-reverse justify-evenly overflow-x-hidden overflow-y-hidden bg-primary text-center outline outline-1 outline-n-dark duration-200 hover:text-xl"
           onClick={newCharacter}
         >
           <h3 className="px-5 text-center">Create a Character</h3>
         </div>
-        <div className="group col-span-3 flex flex-col-reverse overflow-x-hidden overflow-y-hidden outline outline-4 outline-n-dark">
+        <div className="group col-span-3 flex flex-col-reverse overflow-x-hidden overflow-y-hidden outline outline-1 outline-n-dark">
           <img
             src="src/assets/minis.jpg"
             alt="my campaigns"
@@ -47,10 +47,10 @@ function UserDash({ id, firstName, lastName }) {
             My Campaigns
           </h3>
         </div>
-        <div className="flex flex-col-reverse justify-evenly overflow-x-hidden overflow-y-hidden bg-primary px-5 text-center outline outline-4 outline-n-dark transition-all duration-200 hover:text-xl">
+        <div className="flex flex-col-reverse justify-evenly overflow-x-hidden overflow-y-hidden bg-primary px-5 text-center outline outline-1 outline-n-dark transition-all duration-200 hover:text-xl">
           Start a Campaign
         </div>
-        <div className="col-span-4 flex h-16 flex-col-reverse justify-center self-center text-center outline outline-4 outline-n-dark">
+        <div className="col-span-4 flex h-16 flex-col-reverse justify-center self-center text-center outline outline-1 outline-n-dark">
           Join a Campaign
         </div>
       </div>
