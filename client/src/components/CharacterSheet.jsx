07e261dc32a7c9a8ce3cc78edffd407020634ba2
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-04 10:49:05 -0600

    change colors to pallette colors

--- client/src/components/CharacterSheet.jsx
+++ client/src/components/CharacterSheet.jsx
@@ -1,21 +1,37 @@
+import { data } from 'autoprefixer';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 function CharacterSheet() {
+  const { id } = useParams();
+  const [characterData, setCharacterData] = useState();
+
+  useEffect(() => {
+    fetch(`/api/characters/${id}`)
+      .then(r => r.json())
+      .then(data => {
+        console.log(data);
+        setCharacterData(data);
+      });
+  });
+
   return (
-    <div className="aspect-csheet grid h-full w-full grid-cols-9 gap-1">
-      <div className="col-span-3 bg-green-800">Name</div>
-      <div className="bg-green-800">Speed</div>
-      <div className="bg-green-800">AC</div>
-      <div className="bg-green-800">Initiative</div>
-      <div className=" bg-green-800">Proficiency Bonus</div>
-      <div className="col-span-2 bg-green-800">HP</div>
-      <div className="col-start-1 row-span-4 bg-green-800">Ability Scores</div>
-      <div className="col-span-2 row-span-4 bg-green-800">Skills</div>
-      <div className="col-span-3 row-span-4 bg-green-800">Actions</div>
-      <div className="bg-green-800">Spell Attack</div>
-      <div className="bg-green-800">Spell Modifier</div>
-      <div className="bg-green-800">Spell Save DC</div>
-      <div className="col-span-3 row-span-3 bg-green-800">Spells/Abilities</div>
-      <div className="col-span-4 row-span-2 bg-green-800">Saving Throws</div>
-      <div className="col-span-5 row-span-2 bg-green-800">Feats and</div>
+    <div className="aspect-csheet grid h-full w-full grid-cols-9 gap-1 bg-n-dark p-4">
+      <div className="col-span-3 bg-n-light">Name</div>
+      <div className="bg-n-light">Speed</div>
+      <div className="bg-n-light">AC</div>
+      <div className="bg-n-light">Initiative</div>
+      <div className=" bg-n-light">Proficiency Bonus</div>
+      <div className="col-span-2 bg-n-light">HP</div>
+      <div className="col-start-1 row-span-4 bg-n-light">Ability Scores</div>
+      <div className="col-span-2 row-span-4 bg-n-light">Skills</div>
+      <div className="col-span-3 row-span-4 bg-n-light">Actions</div>
+      <div className="bg-n-light">Spell Attack</div>
+      <div className="bg-n-light">Spell Modifier</div>
+      <div className="bg-n-light">Spell Save DC</div>
+      <div className="col-span-3 row-span-3 bg-n-light">Spells/Abilities</div>
+      <div className="col-span-4 row-span-2 bg-n-light">Saving Throws</div>
+      <div className="col-span-5 row-span-2 bg-n-light">Feats and</div>
     </div>
   );
 }
