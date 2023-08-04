2ab7b3b81f708f82f4f7e083bf31687f29a95aa9
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-04 10:57:28 -0600

    change icon depending on open state of sidebar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,6 +7,42 @@ function Header({ user, setUser, sideBar, setSideBar }) {
   const sideBarHidden =
     'absolute left-0 top-0 flex h-16 w-screen justify-center bg-primary px-3 transition-all';
 
+  const hamburger = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="absolute right-2 top-3 h-10 w-10 stroke-white stroke-1 transition-all duration-300 hover:stroke-2"
+      onClick={() => setSideBar(!sideBar)}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
+
+  const closeX = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="absolute right-2 top-3 h-10 w-10 stroke-white stroke-1 transition-all duration-300 hover:stroke-2"
+      onClick={() => setSideBar(!sideBar)}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+
   function logout() {
     setUser(null);
     navigate('/', { replace: false });
@@ -20,22 +56,7 @@ function Header({ user, setUser, sideBar, setSideBar }) {
       >
         Oracle
       </a>
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="absolute right-2 top-3 h-10 w-10 stroke-white stroke-1 transition-all duration-300 hover:stroke-2"
-        onClick={() => setSideBar(!sideBar)}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-        />
-      </svg>
+      {!!sideBar ? closeX : hamburger}
     </div>
   );
 }
