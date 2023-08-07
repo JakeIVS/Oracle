f34d60ce93a286b34577a6a35e6321794c5ab3c6
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-07 15:44:20 -0600

    format bg and hover states

--- client/src/components/NumBox.jsx
+++ client/src/components/NumBox.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function NumBox({ value }) {
-  return <div>{value}</div>;
+  return (
+    <div className="w-fit self-center rounded-md bg-n-light p-2 text-center text-xl font-semibold outline outline-1 hover:bg-n-dark">
+      {value}
+    </div>
+  );
 }
 
 export default NumBox;
