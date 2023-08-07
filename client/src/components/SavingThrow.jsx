89cb70f0d88db517f6d70b9e07d6af61b136d4ee
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-07 11:02:54 -0600

    make saving throw component for use in character sheet

---
+++ client/src/components/SavingThrow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SavingThrow() {
+  return (
+    <div className="overflow-hidden bg-slate-600">
+      <h4>Strength</h4>
+    </div>
+  );
+}
+
+export default SavingThrow;
