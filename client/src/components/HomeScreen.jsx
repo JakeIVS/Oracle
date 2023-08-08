f8492b82b735a5ccd1598f6f587c2f6919a7442b
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-08 15:35:50 -0600

    format logged out screen spacing

--- client/src/components/HomeScreen.jsx
+++ client/src/components/HomeScreen.jsx
@@ -5,17 +5,17 @@ function HomeScreen() {
   const navigate = useNavigate();
 
   return (
-    <div className="h-full w-full bg-gradient-to-t from-n-dark to-primary">
+    <div className="h-full w-full bg-gradient-to-t from-primary to-secondary">
       <h1>You are not signed in</h1>
       <div>
         <button
-          className="button mx-2"
+          className="button mx-2 mt-3 bg-primary"
           onClick={() => navigate('/login', { replace: true })}
         >
           Sign In
         </button>
         <button
-          className="button mx-2"
+          className="button mx-2 bg-primary"
           onClick={() => navigate('/signup', { replace: true })}
         >
           Sign Up
