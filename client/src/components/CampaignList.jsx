828d020e1152229a95d054f321599a8720eed6ac
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-08 14:44:48 -0600

    add id and key to CampaignCard component

--- client/src/components/CampaignList.jsx
+++ client/src/components/CampaignList.jsx
@@ -1,23 +1,20 @@
 import React, { useEffect } from 'react';
+import CampaignCard from './CampaignCard';
 
 function CampaignList({ user }) {
   return (
     <div className="h-full w-full bg-gradient-to-t from-n-light to-n-dark">
       <h1 className="pt-6 text-center font-serif text-3xl font-bold">
-        Your Characters
+        Your Campaigns
       </h1>
       <div className=" mx-[15%] grid grid-cols-1 gap-x-8 gap-y-3 pt-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-        {user.campaigns.map(char => {
+        {user?.campaigns.map(campaign => {
           return (
-            <CharacterCard
-              name={char.name}
-              id={char.id}
-              key={char.id}
-              level={char.level}
-              characterClass={char.character_class}
-              campaign={char.campaign_id}
-              image={char.image_url}
-              race={char.race}
+            <CampaignCard
+              name={campaign.name}
+              characters={campaign.characters}
+              id={campaign.id}
+              key={campaign.id}
             />
           );
         })}
