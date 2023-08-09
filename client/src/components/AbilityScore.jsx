6de8a76c143f28707bd54f6c01b2e0914b02cb4b
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-09 10:22:58 -0600

    change aspect ratio at larger sizes

--- client/src/components/AbilityScore.jsx
+++ client/src/components/AbilityScore.jsx
@@ -3,7 +3,7 @@ import NumBox from './NumBox';
 
 function AbilityScore({ stat, score, bonus }) {
   return (
-    <div className="flex aspect-square flex-col justify-between overflow-hidden bg-n-light pb-3 outline outline-1 lg:aspect-[3/2] xl:aspect-[2/1]">
+    <div className="flex aspect-square flex-col justify-between overflow-hidden bg-n-light pb-3 outline outline-1 lg:aspect-[4/3] xl:aspect-[3/2]">
       <div className="mb-1 flex justify-between border-b-2 border-black">
         <h4 className="w-full bg-n-dark py-1">{stat}</h4>
         <p className="w-fit px-1 font-semibold">{score}</p>
