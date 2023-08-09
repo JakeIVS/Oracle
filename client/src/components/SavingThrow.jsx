a085d8741601d17e3ef87a3a8db9faec1954b9d1
Jacob Ives <jacob.w.ives@gmail.com>
2023-08-09 11:07:01 -0600

    format stat box

--- client/src/components/SavingThrow.jsx
+++ client/src/components/SavingThrow.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import NumBox from './NumBox';
 
-function SavingThrow({ save, bonus }) {
+function SavingThrow({ stat, bonus }) {
   return (
-    <div className="overflow-hidden rounded-sm bg-n-dark px-1 outline outline-1">
-      <h5>Strength</h5>
-      <p>{bonus}</p>
+    <div className="flex justify-between overflow-hidden rounded-sm bg-n-dark outline outline-1">
+      <h5 className="flex flex-col justify-around pl-3 font-serif">{stat}</h5>
+      <div className="flex w-1/4 justify-center rounded-r-sm border-l border-l-black bg-n-light p-1 text-right">
+        <NumBox value={bonus} />
+      </div>
     </div>
   );
 }
