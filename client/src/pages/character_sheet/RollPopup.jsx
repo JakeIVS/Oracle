4b9622ab6b2aadf3e647aa107e92c8450f0059c7
Jacob Ives <jacob.w.ives@gmail.com>
2023-09-12 10:48:25 -0600

    format roll popup

--- client/src/pages/character_sheet/RollPopup.jsx
+++ client/src/pages/character_sheet/RollPopup.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function RollPopup({ roll }) {
   return (
-    <div>
+    <div className="outline-3 absolute bottom-4 left-4 h-44 w-80 rounded-lg bg-gradient-to-t from-n-dark to-n-light shadow-md shadow-black outline">
       <h1>{roll}</h1>
     </div>
   );
