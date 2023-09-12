096f4fd55d027e22c590659c4452c37aaa772e64
Jacob Ives <jacob.w.ives@gmail.com>
2023-09-12 11:31:52 -0600

    hide roll popup unless active

--- client/src/pages/character_sheet/RollPopup.jsx
+++ client/src/pages/character_sheet/RollPopup.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-function RollPopup({ roll }) {
+function RollPopup({ roll, showRoll }) {
+  const active =
+    'outline-3 absolute bottom-4 left-4 h-44 w-80 rounded-lg bg-gradient-to-t from-n-dark to-n-light shadow-md shadow-black outline';
+  const inactive =
+    'absolute bottom-4 left-4 h-44 w-0 rounded-lg bg-gradient-to-t from-n-dark to-n-light shadow-md shadow-black';
   return (
-    <div className="outline-3 absolute bottom-4 left-4 h-44 w-80 rounded-lg bg-gradient-to-t from-n-dark to-n-light shadow-md shadow-black outline">
+    <div className={inactive}>
       <h1>{roll}</h1>
     </div>
   );
